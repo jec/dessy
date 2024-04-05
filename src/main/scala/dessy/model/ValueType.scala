29e304a54bf0b734dc9387a8e1ce76c075e61eab
Jim Cain <jcain@spelt.io>
2024-04-05 01:01:10 -0400

    Set uuid to a default random value

--- src/main/scala/dessy/model/ValueType.scala
+++ src/main/scala/dessy/model/ValueType.scala
@@ -3,5 +3,5 @@ package dessy.model
 trait ValueType {
   val cname: String
   val name: String
-  val uuid: String
+  val uuid: String = java.util.UUID.randomUUID.toString
 }
--- src/test/scala/dessy/acceptance/TraversalSpec.scala
+++ src/test/scala/dessy/acceptance/TraversalSpec.scala
@@ -5,18 +5,18 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.*
 
 class TraversalSpec extends AnyWordSpec with Matchers {
-  case class ObservationType(cname: String, name: String, uuid: String) extends ValueType
-  case class BooleanObservationType(cname: String, name: String, uuid: String) extends BooleanValueType
-  case class EnumeratedObservationType(cname: String, name: String, uuid: String, enumerations: Set[BooleanValueType]) extends EnumeratedValueType
+  case class ObservationType(cname: String, name: String) extends ValueType
+  case class BooleanObservationType(cname: String, name: String) extends BooleanValueType
+  case class EnumeratedObservationType(cname: String, name: String, enumerations: Set[BooleanValueType]) extends EnumeratedValueType
   case class Observation(valueType: ObservationType, value: String) extends Value
 
-  private val basketballSneaker = BooleanObservationType("basketball", "Basketball sneaker", java.util.UUID.randomUUID.toString)
-  private val runningSneaker = BooleanObservationType("running", "Running sneaker", java.util.UUID.randomUUID.toString)
-  private val sneakerType = EnumeratedObservationType("sneaker-type", "Sneaker type", java.util.UUID.randomUUID.toString, Set(basketballSneaker, runningSneaker))
+  private val basketballSneaker = BooleanObservationType("basketball", "Basketball sneaker")
+  private val runningSneaker = BooleanObservationType("running", "Running sneaker")
+  private val sneakerType = EnumeratedObservationType("sneaker-type", "Sneaker type", Set(basketballSneaker, runningSneaker))
 
-  private val dicksSportingGoods = BooleanObservationType("dicks-sporting-goods", "Dick's Sporting Goods", java.util.UUID.randomUUID.toString)
-  private val champsSports = BooleanObservationType("champs-sports", "Champs Sports", java.util.UUID.randomUUID.toString)
-  private val sneakerStore = EnumeratedObservationType("sneaker-store", "Sneaker store", java.util.UUID.randomUUID.toString, Set(dicksSportingGoods, champsSports))
+  private val dicksSportingGoods = BooleanObservationType("dicks-sporting-goods", "Dick's Sporting Goods")
+  private val champsSports = BooleanObservationType("champs-sports", "Champs Sports")
+  private val sneakerStore = EnumeratedObservationType("sneaker-store", "Sneaker store", Set(dicksSportingGoods, champsSports))
 
   "A traversal" should {
     "do something" in {
